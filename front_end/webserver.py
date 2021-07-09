17304f3ec1f212737de7793a208933fb13af73b2
Zach Eliason <eliasonzach@gmail.com>
2021-07-09 14:59:01 -0600

    no redirect

--- front_end/webserver.py
+++ front_end/webserver.py
@@ -1058,21 +1058,27 @@ class CreateVideoExerciseHandler(BaseUserHandler):
 
 class MoveExerciseHandler(BaseUserHandler):
     def post(self, course, assignment, exercise):
+        out_dict = {"result": ""}
         try:
+            print(1)
             if not self.is_administrator() and not self.is_instructor_for_course(course):
                 self.render("permissions.html")
                 return
 
             new_assignment_id = self.get_body_argument("new_assignment_id")
+            print(2, new_assignment_id)
             content.move_exercise(course, assignment, exercise, new_assignment_id)
-            assignment_basics = content.get_assignment_basics(course, assignment)
-
-            assignment_basics = content.get_assignment_basics(course, new_assignment_id)
-            out_file = f"Assignment_{new_assignment_id}_Scores.csv"
-
-            self.render("assignment_admin.html", courses=content.get_courses(True), assignments=content.get_assignments(course, True), exercises=content.get_exercises(course, new_assignment_id, True), exercise_statuses=content.get_exercise_statuses(course, new_assignment_id, self.get_user_info()["user_id"]), course_basics=content.get_course_basics(course), assignment_basics=assignment_basics, assignment_details=content.get_assignment_details(course, new_assignment_id, True), download_file_name=get_scores_download_file_name(assignment_basics), course_options=[x[1] for x in content.get_courses() if str(x[0]) != course], user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor_for_course(course), out_file=out_file)
+            print(3)
+            # assignment_basics = content.get_assignment_basics(course, assignment)
+            #
+            # assignment_basics = content.get_assignment_basics(course, new_assignment_id)
+            # out_file = f"Assignment_{new_assignment_id}_Scores.csv"
+            #
+            # self.render("assignment_admin.html", courses=content.get_courses(True), assignments=content.get_assignments(course, True), exercises=content.get_exercises(course, new_assignment_id, True), exercise_statuses=content.get_exercise_statuses(course, new_assignment_id, self.get_user_info()["user_id"]), course_basics=content.get_course_basics(course), assignment_basics=assignment_basics, assignment_details=content.get_assignment_details(course, new_assignment_id, True), download_file_name=get_scores_download_file_name(assignment_basics), course_options=[x[1] for x in content.get_courses() if str(x[0]) != course], user_info=self.get_user_info(), is_administrator=self.is_administrator(), is_instructor=self.is_instructor_for_course(course), out_file=out_file)
         except Exception as inst:
-            render_error(self, traceback.format_exc())
+            out_dict["result"] = traceback.format_exc()
+        print(4)
+        self.write(json.dumps(out_dict))
 
 class CopyExerciseHandler(BaseUserHandler):
     def post(self, course, assignment, exercise):
